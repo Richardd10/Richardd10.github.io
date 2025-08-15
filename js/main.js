ab13cfaf3f7a78d09a7adb026c50ab59fd6f49b7
Richardd10 <148802393+Richardd10@users.noreply.github.com>
2025-08-15 20:45:05 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// main.js
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navMenu = document.querySelector('.nav-menu');
+
+    mobileMenu.addEventListener('click', function() {
+        mobileMenu.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    });
+});
